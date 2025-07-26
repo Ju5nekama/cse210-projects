eedf3a1640fc0049ba4a0fdce3c0080e41b153a6
ju5nekama <joseibonsu@byupathway.edu>
2025-07-26 22:16:03 +0100

    journal

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public void Display()
+    {
+        DateTime theCurrentTime = DateTime.Now;
+        _date = theCurrentTime.ToShortDateString();
+        Console.WriteLine(_date);
+        Console.WriteLine(_promptText);
+        Console.WriteLine(_entryText);
+
+
+    }
+}
